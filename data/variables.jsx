29eb90e83dd2dbf5920db5103a6d49d5c7ceace4
Roxi96 <roxnazapc@outlook.com>
2022-01-13 11:35:44 -0600

    refactor: adding a data folder for variables

---
+++ data/variables.jsx
@@ -0,0 +1,15 @@
+import bg from '../images/desktop/image-header.jpg'
+
+const heroBg={ 
+    position: 'absolute',
+    backgroundImage: `url(${bg})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    width: '100vw',
+    height: '100vh',
+    top: '0',
+    zIndex: '1'
+  }
+
+  export {heroBg} 
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,21 +1,10 @@
 import React from 'react'
 import classes from './Hero.module.css'
-import bg from '../../../images/desktop/image-header.jpg'
+import { heroBg } from '../../../data/variables'
 
 
 function Hero() {
   
-    const heroBg={ 
-        position: 'absolute',
-        backgroundImage: `url(${bg})`,
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        width: '100vw',
-        height: '100vh',
-        top: '0',
-        zIndex: '1'
-      }
     return (
         <header style={heroBg}>
             <h1 className={classes.title}>WE ARE THE CREATIVES</h1>
