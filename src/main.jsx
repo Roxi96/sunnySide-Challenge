919ceeb0e56e8b6215afd3083418be4e2f473273
Roxi96 <roxnazapc@outlook.com>
2022-01-13 12:27:20 -0600

    refactor: adding Information component

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import Information from './components/Information/Information'
 
 ReactDOM.render(
   <React.StrictMode>
     <Navbar />
     <Hero />
+    <Information />
   </React.StrictMode>,
   document.getElementById('root')
 )
