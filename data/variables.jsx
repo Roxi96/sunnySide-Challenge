6f32bba0bb487e0c13d17450761d5ab71fc9cb59
Roxi96 <roxnazapc@outlook.com>
2022-01-13 12:52:04 -0600

    clean: cleaning lines

--- data/variables.jsx
+++ data/variables.jsx
@@ -1,14 +1,12 @@
 import bg from '../images/desktop/image-header.jpg'
 
 const heroBg={ 
-   
     backgroundImage: `url(${bg})`,
     backgroundPosition: 'center',
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
     width: '99.1vw',
     height: '91.2vh',
-    
   }
 
   export {heroBg} 
