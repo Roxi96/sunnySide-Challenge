ea868f3030fd0801eeebc546b14677ad977fc373
Roxi96 <roxnazapc@outlook.com>
2022-01-13 12:46:56 -0600

    fix: fixing the hero and navbar collion issues

--- data/variables.jsx
+++ data/variables.jsx
@@ -1,15 +1,14 @@
 import bg from '../images/desktop/image-header.jpg'
 
 const heroBg={ 
-    position: 'absolute',
+   
     backgroundImage: `url(${bg})`,
     backgroundPosition: 'center',
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
-    width: '100vw',
-    height: '100vh',
-    top: '0',
-    zIndex: '1'
+    width: '99.1vw',
+    height: '91.2vh',
+    
   }
 
   export {heroBg} 
